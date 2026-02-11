513b31b5167606005d2075ef07c28e27176d2967
Navneetsinghnegi <navneetsingh5647@gmail.com>
2026-02-11 18:59:46 +0530

    task: counter app using useState

---
+++ Day_3/Counter_App/src/Components/Counter.tsx
@@ -0,0 +1,34 @@
+import React, { useState } from 'react'
+
+const Counter = () => {
+    const [count, setCount] = useState<number>(0);
+
+    const increase = ()=>{
+        setCount(prevCount => prevCount+1);
+    }
+
+    const decrease = () => {
+        setCount(prevCount => {
+            if (prevCount === 0) return prevCount;
+            return prevCount - 1;
+        });
+       
+        
+    }
+
+    const reset = ()=>{
+        setCount(0);
+    }
+
+  return (
+    <div>
+        <h1>Counter App</h1>
+        <h3>Count: {count}</h3>
+        <button onClick={()=> increase()}>Increase</button>
+        <button onClick={()=> decrease()}>Decrease</button>
+        <button onClick={()=> reset()}>Reset</button>
+    </div>
+  )
+}
+
+export default Counter
